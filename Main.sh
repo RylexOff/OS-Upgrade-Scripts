b82e4ebe07368a237e594a642521b28d62e4fd79
RylexOff <60745761+RylexOff@users.noreply.github.com>
2023-11-13 17:37:27 +0100

    Update Main.sh
    add fonction install curl and repear

--- Main.sh
+++ Main.sh
@@ -5,6 +5,30 @@ if [ "$EUID" -ne 0 ]; then
   exit 1
 fi
 
+if ! command -v curl &>/dev/null; then
+  echo "curl is not installed. Attempting to install it..."
+  if apt --version >/dev/null 2>&1; then
+    echo "Debian/Ubuntu/Kali Linux/Parrot OS detected. Installing curl..."
+    sudo apt update
+    sudo apt install -y curl
+  elif pacman --version >/dev/null 2>&1; then
+    echo "Arch Linux/Manjaro detected. Installing curl..."
+    sudo pacman -Sy --noconfirm curl
+  elif dnf --version >/dev/null 2>&1; then
+    echo "CentOS/Fedora detected. Installing curl..."
+    sudo dnf install -y curl
+  elif yum --version >/dev/null 2>&1; then
+    echo "CentOS/Fedora detected. Installing curl..."
+    sudo yum install -y curl
+  elif zypper --version >/dev/null 2>&1; then
+    echo "openSUSE detected. Installing curl..."
+    sudo zypper install -y curl
+  else
+    echo "No suitable package manager found for installing curl. Please install curl manually."
+    exit 1
+  fi
+fi
+
 echo "1- Debian/Ubuntu/Kali Linux/Parrot OS Upgrade (apt)"
 echo "2- Arch Linux/Manjaro Upgrade (yay/pacman)"
 echo "3- Fedora/CentOS Upgrade (dnf/yum)"
@@ -17,7 +41,7 @@ choice=${choice:-5}
 
 case $choice in
   1)
-    sudo bash -c "$(curl -fsSL https://raw.githubusercontent.com/RylexOff/OS-Upgrade-Scripts/main/ArchLinux.sh)"
+    sudo bash -c "$(curl -fsSL https://raw.githubusercontent.com/RylexOff/OS-Upgrade-Scripts/main/Debian.sh)"
     ;;
   2)
     sudo bash -c "$(curl -fsSL https://raw.githubusercontent.com/RylexOff/OS-Upgrade-Scripts/main/ArchLinux.sh)"
@@ -31,7 +55,7 @@ case $choice in
   5)
     if apt --version >/dev/null 2>&1; then
       echo "Debian/Ubuntu/Kali Linux/Parrot OS detected."
-      sudo bash -c "$(curl -fsSL https://raw.githubusercontent.com/RylexOff/OS-Upgrade-Scripts/main/DebianUpgrade.sh)"
+      sudo bash -c "$(curl -fsSL https://raw.githubusercontent.com/RylexOff/OS-Upgrade-Scripts/main/Debian.sh)"
     elif yay --version >/dev/null 2>&1; then
       echo "Arch Linux/Manjaro detected."
       sudo bash -c "$(curl -fsSL https://raw.githubusercontent.com/RylexOff/OS-Upgrade-Scripts/main/ArchLinux.sh)"
